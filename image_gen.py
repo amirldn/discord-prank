82800bf272a64b0c822b2373b501922c48464f7a
b66957am <amir.maula@student.manchester.ac.uk>
2021-04-26 00:35:46 +0100

    uploaded to google cloud

--- image_gen.py
+++ image_gen.py
@@ -1,12 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 from imgur import post_image
+import os
 
 # Constants
 dark_bg = ImageColor.getrgb("#36393f")
 dark_text = ImageColor.getrgb("#dcddde")
 whitney = ImageFont.truetype('./whitney/whitneymedium.otf', 40) #16
-message1 = 'Hello world!'
-message2 = 'Hello from mars!'
 
 fly = Image.open('./pranks/fly.png')
 fly.thumbnail((100, 100),Image.ANTIALIAS)
@@ -15,22 +14,25 @@ dark_fly = Image.open('./pranks/dark_fly.jpg')
 width, height = dark_fly.size
 dark_fly.resize((width*2, height*2))
 
-# Create new background
-background = Image.new('RGB', (1000, 105), color=dark_bg)
-d = ImageDraw.Draw(background)
-# Overlay the text
-d.text((0, 0), message1, font=whitney, fill=dark_text)
-d.text((0, 60), message2, font=whitney, fill=dark_text)
+def generate_image(message1='Hello world!', message2='Hello from mars!', prank=fly, color_scheme='dark'):
+    try:
+        # Create new background
+        background = Image.new('RGB', (1000, 105), color=dark_bg)
+        d = ImageDraw.Draw(background)
+        # Overlay the text
+        d.text((0, 0), message1, font=whitney, fill=dark_text)
+        d.text((0, 60), message2, font=whitney, fill=dark_text)
 
-# Set offset and paste
-# third param used to indicate transparency mask
-offset = (800, 0)
-background.paste(dark_fly.resize((width*2, height*2)), offset)
-background.save('./output/output.png')
-try:
-    url = post_image('./output/output.png')
-    print(url)
-except Exception as e:
-    print("An error occured while uploading your image")
-    print(e)
-    quit(0)
+        # Set offset and paste
+        # third param used to indicate transparency mask
+        offset = (800, 0)
+        background.paste(dark_fly.resize((width*2, height*2)), offset)
+        background.save('./output/output.png')
+        try:
+            url = post_image('./output/output.png')
+            os.remove('./output/output.png')
+            return url
+        except Exception as e:
+            return ("An error occured while uploading your image" + str(e))
+    except Exception as e:
+        return ("An error occured while generating your image\n" + str(e))
--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from flask import Flask, request, escape
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import image_gen
+from image_gen import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    message1 = request.args.get("message1", "")
+    message2 = request.args.get("message2", "")
+    if message1:
+        link = image_gen.generate_image(message1=message1, message2=message2, prank='fly', color_scheme='dark')
+        html = ("<a href=%s>%s</a>" % (link, link))
+    else:
+        html = ''
+    return ("""<form action="" method="get">
+                    <p>Message 1</p>
+                    <input type="text" name="message1">
+                    <p>Message 2</p>
+                    <input type="text" name="message2">
+                    <input type="submit" value="Generate">
+                  </form>"""
+    + html)
+
+# @app.route("/<string:message1>,<string:message2>")
+# def generate(message1='message1', message2='message2!'):
+#     # message2 = request.args.get('message2', None)
+#     # prank = request.args.get('prank', None)
+#     # color_scheme = request.args.get('color_scheme', None)
+#     # link = image_gen.generate_image(message1=message1, message2=message2, prank='fly', color_scheme='dark')
+#     link = image_gen.generate_image(message1=message1, message2=message2)
+#     html = ("<a href=%s>%s</a>" % (link,link))
+#     # print(html)
+#     return html
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
