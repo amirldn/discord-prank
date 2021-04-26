7b3f0e3c213d6f62912184ca4843bc5e915cf02e
amirmaula1 <>
2021-04-26 14:47:29 +0000

    added generated count

--- main.py
+++ main.py
@@ -2,34 +2,30 @@ from flask import Flask, request, render_template
 import image_gen
 import random
 from pathlib import Path
+from replit import db
 
 
 app = Flask(__name__, template_folder='template')
+count = int(db["gen_count"])
+
 
 @app.route("/")
 def index():
-    message1 = request.args.get("message1", "")
-    message2 = request.args.get("message2", "")
-    if message1:
-        link = image_gen.generate_image(message1=message1, message2=message2, prank='fly', color_scheme='dark')
-        # html = ("<a href=%s>%s</a>" % (link, link))
-    else:
-        # html = ''
-        link = ''
-    return render_template('index.html', link=link)
-    # return ("""<form action="" method="get">
-    #                 <p>Message 1</p>
-    #                 <input type="text" name="message1">
-    #                 <p>Message 2</p>
-    #                 <input type="text" name="message2">
-    #                 <input type="submit" value="Generate">
-    #               </form>"""
-    # + link)
+  global count
+  message1 = request.args.get("message1", "")
+  message2 = request.args.get("message2", "")
+  if message1:
+      link = image_gen.generate_image(message1=message1, message2=message2, prank='fly', color_scheme='dark')
+      count += 1
+      db["gen_count"] = str(count)
+  else:
+      link = ''
+  return render_template('index.html', link=link, count=count)
 
 
 if __name__ == "__main__":
   Path("./output/").mkdir(parents=True, exist_ok=True)
   app.run(
-		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+		host='0.0.0.0',
+		port=random.randint(2000, 9000)
 	)
