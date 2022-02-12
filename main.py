2c00267e7100995074c44f4894312936bf144b4f
Amir Maula <62271191+amirmaula@users.noreply.github.com>
2022-02-12 20:05:20 +0000

    Updated logging

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from flask import Flask, request, render_template
 import image_gen
-# import random
-from random import randint
 from pathlib import Path
 from replit import db
 import logging
-    
-
-print("Saved client_id")
 
 app = Flask(__name__, template_folder='template')
 count = int(db["gen_count"])
-
 
 @app.route("/")
 def index():
@@ -22,6 +16,7 @@ def index():
       link = image_gen.generate_image(message1=message1, message2=message2, prank='fly', color_scheme='dark')
       count += 1
       db["gen_count"] = str(count)
+      logging.info("Message 1: {m1}, Message 2: {m2}, Link: {link}".format(m1=message1, m2=message2, link=link))
   else:
       link = ''
   return render_template('index.html', link=link, count=count)
@@ -30,8 +25,5 @@ def index():
 if __name__ == "__main__":
   Path("./output/").mkdir(parents=True, exist_ok=True)
   logging.basicConfig(filename='info.log',level=logging.INFO)
-  app.run(
-		host='0.0.0.0',
-		port=randint(2000, 9000),
-    debug=False
-	)
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8080)
